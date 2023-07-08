afe0e94c98644ce430ce9babc358aff262ba73f4
wmr <mingrui@whut.edu.cn>
2023-07-08 22:23:05 +0800

    完善管理员对于图书的操作（增加，删除，更新），此部分完成

--- administrator.cpp
+++ administrator.cpp
@@ -25,6 +25,12 @@ Administrator::Administrator(unsigned id,QString name,QWidget *parent) :
     //关于排序书本
     connect(ui->ascBookOrder,&QRadioButton::toggled,this,&Administrator::ascBookOrderFun);
     connect(ui->descBookOrder,&QRadioButton::toggled,this,&Administrator::descBookOrderFun);
+    //删除书本
+    connect(ui->delBookButton,&QPushButton::clicked,this,&Administrator::delBookFun);
+    //插入书本
+    connect(ui->addBookButton,&QPushButton::clicked,this,&Administrator::insertBookFun);
+    //更新书本
+    connect(ui->updateBookButton,&QPushButton::clicked,this,&Administrator::updateBookFun);
 }
 
 Administrator::~Administrator()
@@ -46,7 +52,6 @@ void Administrator::bookGraph()
     bookTable->select();
     ui->tableViewBook->setModel(bookTable);
     ui->tableViewBook->setEditTriggers(QAbstractItemView::NoEditTriggers);//不可编辑
-
 
 }
 
@@ -98,11 +103,94 @@ void Administrator::descBookOrderFun()
  */
 void Administrator::delBookFun()
 {
+    int curRow=ui->tableViewBook->currentIndex().row();
+    if(ui->isbnEdit->text().isEmpty()&&curRow==-1)//错误的操作
+    {
+        QMessageBox::warning(this,"警告","未选定删除对象，请重新操作");
+        return;
+    }
+    int ret = QMessageBox::warning(this, "删除", "你确定要删除该行吗", QMessageBox::Yes | QMessageBox::No);
+    if(ret==QMessageBox::No)
+        return;
     if(ui->isbnEdit->text().isEmpty())
     {
         int curRow=ui->tableViewBook->currentIndex().row();  //获取选定行
+        bookTable->removeRow(curRow);
+        bookTable->select();
     }else{
-
+        //判断是否存在该书本
+        // 准备查询
+        MysqlServer::getInstance()->getQuery()->prepare("select * from book_info where isbn = :id");
+        MysqlServer::getInstance()->getQuery()->bindValue(":id", ui->isbnEdit->text().toInt());
+        // 执行查询
+        if (MysqlServer::getInstance()->getQuery()->exec()) {
+            // 检查查询结果是否为空
+            if (MysqlServer::getInstance()->getQuery()->next()) {
+                // 如果结果不为空，那么书存在，可以进行删除操作
+                MysqlServer::getInstance()->getQuery()->prepare("delete from book_info where isbn = :id");
+                MysqlServer::getInstance()->getQuery()->bindValue(":id", ui->isbnEdit->text().toInt());
+                if (MysqlServer::getInstance()->getQuery()->exec()) {
+                    bookTable->select();
+                }
+            } else {
+                //如果结果为空，那么书不存在，显示错误消息
+                QMessageBox::warning(this, "错误", "不存在该书");
+            }
+        }
     }
-    bookTable->removeRow(curRow);
+}
+/*
+ * 插入操作：不能isbn重复
+ *
+ */
+void Administrator::insertBookFun()
+{
+    QString newIsbn = ui->isbnEdit->text();
+    // 先检查书是否已存在
+    MysqlServer::getInstance()->getQuery()->prepare("SELECT * FROM book_info where isbn = :id");
+    MysqlServer::getInstance()->getQuery()->bindValue(":id", newIsbn);
+    MysqlServer::getInstance()->getQuery()->exec();
+    if ( MysqlServer::getInstance()->getQuery()->next()) {//说明确实有数据
+        QMessageBox::warning(this, "错误", "插入isbn不能相同");
+        return;
+    }
+    // 执行插入操作
+    int rowNum=bookTable->rowCount();
+    bookTable->insertRow(rowNum);
+    bookTable->setData(bookTable->index(rowNum,0),newIsbn);
+    bookTable->setData(bookTable->index(rowNum,1),ui->booknameEdit->text());
+    bookTable->setData(bookTable->index(rowNum,2),ui->authorEdit->text());
+    bookTable->setData(bookTable->index(rowNum,3),ui->publisherEdit->text());
+    bookTable->setData(bookTable->index(rowNum,4),QDateTime::currentDateTime().toString("yyyy-MM-dd HH:mm:ss"));
+    // 提交改动
+    bookTable->submitAll() ;
+}
+
+void Administrator::updateBookFun()
+{
+    QString newIsbn = ui->isbnEdit->text();
+    int rowNum = -1;
+    for (int i = 0; i < bookTable->rowCount(); ++i)
+    {
+        if (bookTable->data(bookTable->index(i, 0)).toString() == newIsbn)
+        {
+            rowNum = i;
+            break;
+        }
+    }
+    if (rowNum == -1)
+    {
+       int ret = QMessageBox::warning(this, "错误", "未找到该isbn，是否进行添加操作", QMessageBox::Yes | QMessageBox::No);
+       if(ret==QMessageBox::No)
+            return;
+       else{
+           this->insertBookFun();
+           return;
+       }
+    }
+    // 更新数据
+    bookTable->setData(bookTable->index(rowNum,1),ui->booknameEdit->text());
+    bookTable->setData(bookTable->index(rowNum,2),ui->authorEdit->text());
+    bookTable->setData(bookTable->index(rowNum,3),ui->publisherEdit->text());
+    bookTable->setData(bookTable->index(rowNum,4),QDateTime::currentDateTime().toString("yyyy-MM-dd HH:mm:ss"));
 }
--- administrator.h
+++ administrator.h
@@ -4,6 +4,7 @@
 #include <QWidget>
 #include <QListWidget>
 #include <QStringList>
+#include <mysqlServer.h>
 #include <book.h>
 #include <QVector>
 #include <QSqlTableModel>
@@ -27,11 +28,14 @@ private:
     void initGraph();
     void bookGraph();
     void logGraph();
+    //图书操作
     void queryBookFunction();//查询的槽函数
     void queryAllBookFunction();//查询的槽函数
     void ascBookOrderFun();//升序
     void descBookOrderFun();//降序
     void delBookFun(); //删除
+    void insertBookFun();//插入
+    void updateBookFun();//更新
 
     unsigned id;//工号
     QString name;//姓名
